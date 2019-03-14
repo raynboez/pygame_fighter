08d4425814f6260951c0a04ae4fbe5072d211afd
Tayma <32221971+taymz-f@users.noreply.github.com>
2019-03-14 13:48:53 +0000

    Set background class as the background of the program. Set it correctly in game loop.

--- Fighter/Background.py
+++ Fighter/Background.py
@@ -3,15 +3,15 @@ try:
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-class background:
-    def __init__(self, canvas):
+class Background:
+    def __init__(self):
         pass
 
-    def draw(canvas):
+    def draw(self, canvas):
         canvas.draw_line((0, 50), (500, 50), 100,'rgb(0,191,255)') #sky
         canvas.draw_line((0, 250), (500, 250), 300,'White') #middle
         canvas.draw_line((0, 450), (500, 450), 100, 'rgb(205,201,201)') #platform and below
 
-    frame = simplegui.create_frame("Background", 500, 500)
-    frame.set_draw_handler(draw)
-    frame.start()
+    #frame = simplegui.create_frame("Background", 500, 500)
+    #frame.set_draw_handler(draw)
+    #frame.start()

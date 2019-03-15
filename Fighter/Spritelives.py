feea9a13f9747dee04f136fd9930d7f192d82089
Tayma <32221971+taymz-f@users.noreply.github.com>
2019-03-15 11:47:44 +0000

    Lifebar outputs correct lives when passed as a parameter.

--- Fighter/Spritelives.py
+++ Fighter/Spritelives.py
@@ -9,24 +9,16 @@ class Spritelives:
         self.image = simplegui._load_local_image('.\\Sprites\\lives_sprite.png')
         self.pos = pos
         self.size = (90,30)
-        self.dims = (3,1) #columns x rows
         self.window = (30, 30)
-        self.current = [2,0]
+        self.current = [0,0]
+        self.center = [self.window[0]/2, self.window[1]/2]
         self.lives = lives
 
-
     def draw(self, canvas):
-        self.framewidth = 30 # can change this to self.window[0]
-        self.frameheight = 30
-        self.frameCentreX = (self.lives * self.framewidth/2)
-        self.frameCentreY = (self.frameheight/2)
-        #self.dims = self.size / 2
-        #canvas.draw_image(image, center_source, width_height_source, center_dest, width_height_dest)
-        canvas.draw_image(self.image, (self.frameCentreX, self.frameCentreY), self.size, self.pos, (90,30))
-        #canvas.draw_image(self.image, (150, 100), self.window, (200, 200), self.window)
-        #canvas.draw_image(self.image, (self.size[0]/2, self.size[1]/2), self.size, (200,200), (90, 30))
+         canvas.draw_image(self.image, ((self.center[0] * self.lives), self.center[1]), (self.window[0]*self.lives, self.window[1]), self.pos, (self.window[0]*self.lives, self.window[1]))
 
-sprite1 = Spritelives((150, 150), 3)
+
+sprite1 = Spritelives((100, 200), 3)
 
 
 def draw(canvas):

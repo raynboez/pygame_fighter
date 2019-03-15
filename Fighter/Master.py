24ef81a7a816cc1a820478474c30d3b600ab1f36
raynboez <Benjamin.Davies.2018@live.rhul.ac.uk>
2019-03-15 15:27:01 +0000

    Master file that creates the frame to be used, and will move the game through its stages

---
+++ Fighter/Master.py
@@ -0,0 +1,25 @@
+try:
+    import simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+
+from Fighter import GameLoop
+
+
+frame = simplegui.create_frame("NAME", 500, 500)
+
+def runGame():
+    #menu()
+    #SpriteSelect()
+    gameLoop()
+
+
+def menu():
+    #MainMenu.init()
+    pass
+def spriteSelect():
+    #SpriteSelect.init()
+    pass
+
+def gameLoop():
+    GameLoop.init()

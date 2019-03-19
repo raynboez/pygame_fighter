6fb25f7f4c1fedbe15543ecfd42f05f93ea3770d
raynboez <Benjamin.Davies.2018@live.rhul.ac.uk>
2019-03-19 16:19:17 +0000

    Cleanup of some redundancies

--- Fighter/Background.py
+++ Fighter/Background.py
@@ -11,7 +11,3 @@ class Background:
         canvas.draw_line((0, 50), (500, 50), 100,'rgb(0,191,255)') #sky
         canvas.draw_line((0, 250), (500, 250), 300,'White') #middle
         canvas.draw_line((0, 450), (500, 450), 100, 'rgb(205,201,201)') #platform and below
-
-    #frame = simplegui.create_frame("Background", 500, 500)
-    #frame.set_draw_handler(draw)
-    #frame.start()

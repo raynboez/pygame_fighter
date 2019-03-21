7790ffd64a1c5008a9074708f3c23a107668933e
raynboez <Benjamin.Davies.2018@live.rhul.ac.uk>
2019-03-21 12:37:56 +0000

    sprite class completed (hopefully)

--- Fighter/Sprite.py
+++ Fighter/Sprite.py
@@ -4,10 +4,24 @@ except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 
+states = {
+        "idle"  : [0, 2],
+        "walk"  : [2, 2],
+        "jump"  : [12, 1],
+        "kick"  : [13, 1],
+        "hit"   : [11, 1],
+        "punch" : [4, 2],
+        "fire"  : [6, 2],
+        "block" : [10, 1],
+        "die"   : [8, 2],
+        "fireball" : [0, 4]
+    }
+
+
 class Sprite:
 
     #initialises sprite from local path, with spritesheet height and width, number of rows and columns, destination and size
-    def __init__(self, path, width, height, rows, columns, destination, scale):
+    def __init__(self, path, width, height, rows, columns, destination, scale, state):
         self.img = simplegui.load_image(path)
         self.spriteDim = [(width / columns), height / rows]
         self.dest = destination
@@ -15,16 +29,20 @@ class Sprite:
         self.columns = columns
         self.rows = rows
         self.frameIndex = [0, 0]
+        self.currentState = states[state]
+        self.timer = 0
 
     #sets destination of sprite (used in character class to update position
     def setDest(self, dest):
         self.dest = dest
 
+    def changeState(self, state):
+        self.currentState = states[state]
+
     #moves to nextSprite
     def nextSprite(self):
         self.frameIndex[0] = (self.frameIndex[0] + 1) % self.columns
-        if self.frameIndex[0] == 0:
-            self.frameIndex[1] = (self.frameIndex[1] + 1) % self.rows
+
 
     #draws sprite on canvas
     def draw(self, canvas):
@@ -40,4 +58,6 @@ class Sprite:
     def update(self, canvas, position):
         self.setDest(position)
         self.draw(canvas)
-        self.nextSprite()
+        if self.timer % 6 == 0:
+            self.nextSprite()
+        self.timer += 1

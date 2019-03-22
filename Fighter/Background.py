588d468d82cc08744ddf45db9c3287002392fb41
Tayma <32221971+taymz-f@users.noreply.github.com>
2019-03-22 13:42:37 +0000

    Win message displayed before game end. Removed some comments.

--- Fighter/Background.py
+++ Fighter/Background.py
@@ -8,10 +8,6 @@ class Background:
         self.image = simplegui.load_image('https://i.ibb.co/ZmbB0Jr/backing.png')
         self.img_size = (640, 253)
 
-
     def draw(self, canvas):
-        #canvas.draw_line((0, 50), (500, 50), 100,'rgb(0,191,255)') #sky
-        #canvas.draw_line((0, 250), (500, 250), 300,'White') #middle
-        #canvas.draw_line((0, 450), (500, 450), 100, 'rgb(205,201,201)') #platform and below
         canvas.draw_image(self.image, (self.img_size[0]//2, self.img_size[1]//2), self.img_size, (250,250), (500,500))
 
--- Fighter/Rounds.py
+++ Fighter/Rounds.py
@@ -6,13 +6,16 @@ except ImportError:
 timer = 0
 delay = 60
 
+
 class Rounds:
 
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.setEnd = False
         #self.maxrounds = maxrounds
         self.gameEnded = False
+        self.startTimer = 0
     def start(self):
         self.roundNum = 1
 
@@ -31,9 +34,22 @@ class Rounds:
     def nextRound(self):
         self.player1.newLife()
         self.player2.newLife()
+        self.startTimer = 0
 
     def end(self, character):
         self.gameEnded = True
-        #print(character.p_number + " has died")
-        #canvas.draw_text(text + "has won!", text_pos, 12, 'Blue')
-        #return to main menu
+        self.deadchar = character
+        if not self.setEnd:
+            self.endTime = self.startTimer
+            self.setEnd = True
+
+    def draw(self, canvas):
+        canvas.draw_text('Round ' + str(self.roundNum), (180, 200), 50, 'White')
+
+    def drawend(self, canvas):
+        if self.deadchar.p_number == 1:
+            self.winner = 'Player 2'
+        else:
+            self.winner = 'Player 1'
+        canvas.draw_text(self.winner + " has won!", (120,200), 40, 'White')
+

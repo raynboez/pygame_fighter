99166fc51026e85f312f4cf2dab806103673e0de
raynboez <Benjamin.Davies.2018@live.rhul.ac.uk>
2019-03-22 18:28:25 +0000

    updated bugs and improvements list

--- Fighter/Instructions.py
+++ Fighter/Instructions.py
@@ -35,7 +35,7 @@ def instructions(canvas):
 def draw(canvas):
     instructions(canvas)
     if kbd.next:
-        Master.gameLoop("Yellow", "Blue")
+        Master.gameLoop("Red", "Blue")
 
 kbd = InstructionsKeyboard()
 

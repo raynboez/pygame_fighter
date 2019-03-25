5620b0ce47fc7338cf884fbe8312de53eb0d3f98
jmon99 <48354753+jmon99@users.noreply.github.com>
2019-03-25 14:28:28 +0000

    Create Music.py

---
+++ Fighter/Music.py
@@ -0,0 +1,15 @@
+import simplegui
+
+class Music:
+  def __init__:
+    self.menu = simplegui.load_sound("https://raw.githubusercontent.com/raynboez/pygame_fighter/master/Fighter/music/Maz%20Nimra%20-%20Level%20One.ogg")
+    self.fight = simplegui.load_sound("https://raw.githubusercontent.com/raynboez/pygame_fighter/master/Fighter/music/07%20Battle%20of%20Pogs.mp3")
+    
+    self.menu.set_volume(1)
+    self.fight.set_volume(1)
+    
+  def play(music):
+    music.play()
+  
+  def pause(music):
+    music.pause()

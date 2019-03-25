e039109ec80a00c575c02980667f26e53fb313cc
jmon99 <48354753+jmon99@users.noreply.github.com>
2019-03-25 12:49:33 +0000

    Update SpriteSelect.py

--- Fighter/SpriteSelect.py
+++ Fighter/SpriteSelect.py
@@ -36,7 +36,7 @@ cursor2.addPos([250, 255], [250, 420], [375, 420], [375, 255], 1)
 player1 = Navigation(cursor1, kbd, 0)
 player2 = Navigation(cursor2, kbd, 1)
 
-selection = Sprite(".//Sprites//full.png", 247, 322, 1, 1, [250, 250], 1)
+selection = StaticSprite(".//Sprites//full.png", 247, 322, 1, 1, [250, 250], 1)
 
 previewSprites = [".//Sprites//redsheet.png", ".//Sprites//bluesheet.png", ".//Sprites//greensheet.png", ".//Sprites//yellowsheet.png"]
 names = [".//Sprites//redPlaceHolder.png", ".//Sprites//bluePlaceHolder.png", ".//Sprites//greenPlaceHolder.png", ".//Sprites//yellowPlaceHolder.png"]
@@ -60,8 +60,8 @@ def drawSpriteSheet(canvas):
     #previewSprite1.draw(canvas)
     #previewSprite2.draw(canvas)
 
-    Sprite(previewSprites[cursor1.previewSelection()], 210, 52, 2, 14, (50, 250), 8).draw(canvas)
-    Sprite(previewSprites[cursor2.previewSelection()], 210, 52, 2, 14, (450, 250), 8).draw(canvas)
+    Sprite(previewSprites[cursor1.previewSelection()], 210, 52, 2, 14, (50, 250), 8).draw(canvas, "idle", "left")
+    Sprite(previewSprites[cursor2.previewSelection()], 210, 52, 2, 14, (450, 250), 8).draw(canvas, "idle", "right")
 
     if cursor1.getSelected():
         Sprite(names[cursor1.previewSelection()], 400, 15, 2, 14, (50, 50), 4).draw(canvas)
